b287ca02b7c74a57a61e03c06c1d3d1d4816904c
Daniel Guerra <jdguerra26@gmail.com>
2022-10-13 18:53:40 -0500

    version final

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const showMenuMobile = () => {
-  const nav = document.getElementById('nav')
-  const bgNav = document.getElementById('bg-nav-mobile')
-  nav.classList.add('nav-active')
-  bgNav.classList.add('bg-active')
-}
+  const nav = document.getElementById('nav');
+  const bgNav = document.getElementById('bg-nav-mobile');
+  nav.classList.add('nav-active');
+  bgNav.classList.add('bg-active');
+};
 
 const hideMenuMobile = () => {
-  const nav = document.getElementById('nav')
-  const bgNav = document.getElementById('bg-nav-mobile')
-  nav.classList.remove('nav-active')
-  bgNav.classList.remove('bg-active')
-}
+  const nav = document.getElementById('nav');
+  const bgNav = document.getElementById('bg-nav-mobile');
+  nav.classList.remove('nav-active');
+  bgNav.classList.remove('bg-active');
+};
